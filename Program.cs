4ef289cc2e4e1b44304a011a2c1c974f34b34c16
eterity88 <111513639+eterity88@users.noreply.github.com>
2022-10-14 20:20:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// лучше обойтись исключительно массивами.
+
+
+string[] M;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            M = stroka.Split(' ');
+            var result = new string[M .Length];
+            var arrSize = 0;
+            foreach (var value in M )
+            {
+                if (value.Length <= 3)
+                {
+                    result[arrSize] = value;
+                    arrSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, arrSize));
+            Console.ReadKey(true);
